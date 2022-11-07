f8a0e7b0e24579322ee55a91814e308cf69219cb
Seo-young Choi <105257804+achime310@users.noreply.github.com>
2022-11-07 11:15:06 +0900

    Create 3진법뒤집기.java

---
+++ 3진법뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+     
+        String s= Integer.toString(n,3);
+
+        String temp="";
+        for(int i=s.length()-1; i>-1;i--){
+            temp+=s.charAt(i);
+        }
+     
+        return Integer.parseInt(temp,3);
+    }
+}
