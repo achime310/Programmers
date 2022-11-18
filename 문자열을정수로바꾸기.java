0723272563a4fbc7ca38b28f024570017947bc38
Seo-young Choi <105257804+achime310@users.noreply.github.com>
2022-11-18 14:59:24 +0900

    Create 문자열을정수로바꾸기.java

---
+++ 문자열을정수로바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        if(s.startsWith("-")) answer = -1*Integer.parseInt(s.substring(1));
+        else answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
