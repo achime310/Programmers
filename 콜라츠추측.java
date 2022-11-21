59dcea86c7c26c2fe61508cec2a353e602cd44c3
Seo-young Choi <105257804+achime310@users.noreply.github.com>
2022-11-22 00:28:50 +0900

    Create 콜라츠추측.java

---
+++ 콜라츠추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = (long)num;
+        
+        while(n!=1){
+            answer++;
+            if(n%2==0){
+                n=n/2;    
+            }else{
+                n=n*3+1;
+            }
+            System.out.println(n);
+        }
+        
+        if(answer>=500) answer=-1;
+        
+        return answer;
+    }
+}
